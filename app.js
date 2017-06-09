02c08abfb7eb79df87546f08e085c7b9b9b53f8b
Cole Allen <decolesss@gmail.com>
2017-06-08 20:04:32 -0700

    added location obect literals

---
+++ app.js
@@ -0,0 +1,29 @@
+'use strict';
+
+//creating object literals
+
+var loc1stAndPike = {
+  minCustHourly: 23
+  maxCustHourly: 65
+  avgCookiesPerCust: 6.3
+}
+var locSeaTacAirport = {
+  minCustHourly: 3
+  maxCustHourly: 24
+  avgCookiesPerCust: 1.2
+}
+var locSeattleCenter = {
+  minCustHourly: 11
+  maxCustHourly: 38
+  avgCookiesPerCust: 3.7
+}
+var locCapitalHill = {
+  minCustHourly: 20
+  maxCustHourly: 38
+  avgCookiesPerCust: 2.3
+}
+var locAlki = {
+  minCustHourly: 2
+  maxCustHourly: 16
+  avgCookiesPerCust: 4.6
+}
