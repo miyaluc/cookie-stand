1a9f26c1c4f50025275cdfc71c39a4453aa868c7
Cole Allen <decolesss@gmail.com>
2017-06-13 21:34:53 -0700

    fixed table printing error, used wrong var

--- app2.js
+++ app2.js
@@ -4,72 +4,71 @@ var hoursOpen = ['06:00am', '07:00am', '08:00am', '09:00am', '10:00am', '11:00am
 var allTheStores = [];
 
 //creating object literals
-function Store(name, minCust, maxCust, avgCookiesSale, hoursOpen) {
+function Store(name, minCust, maxCust, avgCookiesSale) {
   this.name = name;
   this.minCust = minCust;
   this.maxCust = maxCust;
   this.avgCookiesSale = avgCookiesSale;
-  this.hoursOpen = hoursOpen;
   this.hourlySales = [];
   allTheStores.push(this);
 }
+//too many tables, how to make rows more dynamic?
+//function to push hours onto
 
 var tableEl = document.getElementById('table');
 var tableBodyEl = document.createElement('tbody');
-var newRowEl = document.createElement('tr');
-var DataEl = document.createElement('td');
 tableEl.appendChild(tableBodyEl);
 
-function makeRow(store) {
-  newRowEl.appendChild(tableBodyEl)
+function makeHeader() {
+  var headerEl = document.createElement('thead');
+  tableEl.appendChild(headerEl);
 
-  DataEl.textContent = store.name;
-  newRowEl.appendChild(DataEl);
+  var locationEl = document.createElement('th');
+  locationEl.textContent = 'Store Locations';
+  headerEl.appendChild(locationEl);
 
-  var hoursOpenDataEl = document.createElement('td');
-  hoursOpenDataEl.textContent = store.hoursOpen;
-  newRowEl.appendChild(hoursOpenDataEl);
-
-  var avgCookiesSaleDataEl = document.createElement('td');
-  avgCookiesSaleDataEl.textContent = store.avgCookiesSale;
-  newRowEl.appendChild(avgCookiesSaleDataEl);
-
-  newRowEl.appendChild(tableBodyEl);
-  document.write(table);
+  for (var i = 0; i < hoursOpen.length; i++) {
+     var hoursOpenDataEl = document.createElement('th');
+     hoursOpenDataEl.textContent = hoursOpen[i];
+     headerEl.appendChild(hoursOpenDataEl);
+  }
 }
-//tableBodyEl.appendChild(table);
 
-var pike = new Store('1st and Pike', 23, 65, 6.5, hoursOpen);
-var seaTac = new Store('SeaTac Airport', 3, 24, 1.2, hoursOpen);
-var seaCenter = new Store('Seattle Center', 11, 38, 3.7, hoursOpen);
-var theHill = new Store('Capital Hill', 20, 38, 2.3, hoursOpen);
-var alki = new Store('Alki', 2, 16, 4.6, hoursOpen);
+function makeRow(store) {
+  var newRowEl = document.createElement('tr');
+  tableBodyEl.appendChild(newRowEl);
+
+  var dataEl = document.createElement('td');
+  dataEl.textContent = store.name;
+  newRowEl.appendChild(dataEl);
+
+  for (var i = 0; i < store.hourlySales.length; i++) {
+    var avgCookiesSaleDataEl = document.createElement('td');
+    avgCookiesSaleDataEl.textContent = store.hourlySales[i];
+    newRowEl.appendChild(avgCookiesSaleDataEl);
+  }
+}
+
+var pike = new Store('1st and Pike', 23, 65, 6.5);
+var seaTac = new Store('SeaTac Airport', 3, 24, 1.2);
+var seaCenter = new Store('Seattle Center', 11, 38, 3.7);
+var theHill = new Store('Capital Hill', 20, 38, 2.3);
+var alki = new Store('Alki', 2, 16, 4.6);
 
 var randomNum = function(store) {
   return Math.floor(Math.random() * (store.maxCust - store.minCust + 1) + store.minCust);
 }
 
 var genHourlySales = function(store) {
-   for (var i = 0; i < store.hoursOpen.length; i++) {
-     store.hourlySales.push(randomNum(store) * store.avgCookiesSale);
-   }//this is storing data in the function and not in the object, won't stick around, figure that out (maybe create an array within constructor object)
- }
-
-// loop through entire allTheStores object, containing Stores.
-for (var i = 0; i < allTheStores.length; i++) {
-  // allTheStores[i].name;
-  // var thisStoreHourlySales = genHourlySales(allTheStores[i]);
-  // thisStoreHourlySales;
-
-  // Print out a row (tr) for this store
-  makeRow(allTheStores[i]);
-
+  for (var i = 0; i < hoursOpen.length; i++) {
+    store.hourlySales.push(randomNum(store) * Math.floor(store.avgCookiesSale));
+  }
+  console.log(store.hourlySales)
 }
 
-//is not being stored in constructor object
-//genHourlySales(loc1stAndPike);
+makeHeader();
 
-  //allStores)
-
-//console.log(genHourlySales(loc1stAndPike));
-//console.log(loc1stAndPike);
+for (var i = 0; i < allTheStores.length; i++) {
+  genHourlySales(allTheStores[i]);
+  makeRow(allTheStores[i]);
+}
